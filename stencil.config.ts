7f55200e8d9ed7bae507355305ad02344596b1eb
Amruta Rajiv Bellikatti <amruta.bellikatti@gmail.com>
2020-02-25 16:24:02 +1300

    Removed private field from package.json and removed other private files

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,32 +1,18 @@
 import { Config } from '@stencil/core';
-import { stylus } from '@stencil/stylus';
-import { getBuildMeta } from './src/plugins/build-meta';
-import { getEnvMeta, resolveEnv } from './src/plugins/resolve-env';
-
-const { gitBranch } = getEnvMeta();
 
 /**
  * @see https://stenciljs.com/docs/config
  *
- * @todo use injectGlobalPaths (or maybe includePaths) for stylus in combination
- * with environment variables in order to apply variables for different themes
- * @see https://github.com/ionic-team/stencil-stylus#inject-globals-stylus-paths
  */
 export const config: Config = {
 	minifyJs: false,
 	namespace: 'stencil-helpers',
-	devMode: gitBranch ? !['master', 'prod'].includes(gitBranch) : undefined,
-	plugins: [getBuildMeta(), resolveEnv(), stylus()],
 	outputTargets: [
 		{
 			type: 'www',
 			serviceWorker: { swSrc: 'src/sw.js', unregister: false },
 
 			copy: [
-				{
-					src: 'assets/icon/favicon.ico',
-					dest: 'favicon.ico',
-				},
 				/**
 				 * Copy Moment.js into the output target so it can be referenced as a
 				 * script from `index.html` rather than being bundled along with the
@@ -48,8 +34,6 @@ export const config: Config = {
 			],
 		},
 	],
-	globalScript: 'src/global/global.ts',
-	globalStyle: 'src/global/global.styl',
 	testing: {
 		roots: ['<rootDir>/__mocks__', '<rootDir>/src'],
 		setupFilesAfterEnv: ['<rootDir>/jest-setup-file.js'],
